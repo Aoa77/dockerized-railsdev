6e3b8a9172d8281acb2b4b52e5e4a6a48b8d2ec9
aoa77 <alalbers77@gmail.com>
2024-09-04 11:47:37 -0500

    launch script

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# rails-ubuntu-dev-container
+
+# dockerized-railsdev
 A Ruby on Rails template repository. Uses docker to bootstrap a containerized Ubuntu environment for Rails development.  
+
+```
+sudo usermod -a -G docker $USER
+grep docker /etc/group
+newgrp docker
+code .
+```
--- dockerize.sh
+++ dockerize.sh
@@ -1,5 +1,26 @@
-clear
+function splash {
+    clear
+    
+#       _            _             _             _                 _ _         _            
+#    __| | ___   ___| | _____ _ __(_)_______  __| |      _ __ __ _(_) |___  __| | _____   __
+#   / _` |/ _ \ / __| |/ / _ \ '__| |_  / _ \/ _` |_____| '__/ _` | | / __|/ _` |/ _ \ \ / /
+#  | (_| | (_) | (__|   <  __/ |  | |/ /  __/ (_| |_____| | | (_| | | \__ \ (_| |  __/\ V / 
+#   \__,_|\___/ \___|_|\_\___|_|  |_/___\___|\__,_|     |_|  \__,_|_|_|___/\__,_|\___| \_/  
+                                                                                          
+                                                                                            
+     echo "Dockerize Rails Development Environment"
+     echo "========================================="
+     echo ""
+     echo "This script will build a docker image for Rails development."
+     echo "It will also run a container with the image."
+     echo ""
+     echo "Press any key to continue..."
+     read -n 1 -s   
+}
+
+splash
 docker rm railsdev --force
 docker image rm ubuntu:aoa77-rails-dev
 docker build -t ubuntu:aoa77-rails-dev .
+
 docker run --name railsdev -it ubuntu:aoa77-rails-dev
